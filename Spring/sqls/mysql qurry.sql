18e76f8f84824f4c4e4c6dd75f62bad7a29397c3
hedwig <hedwig3798@gmail.com>
2022-01-17 18:49:33 +0900

    sql qurry

---
+++ Spring/sqls/mysql qurry.sql
@@ -0,0 +1,15 @@
+create database boxfolio default character set utf8;
+
+use boxfolio;
+
+show tables;
+
+select * from userInfo;
+
+create table userInfo(
+	id varchar(20) not null,
+    nickName varchar(10) not null,
+    userLevel tinyint unsigned default 0,
+    pw varchar(64) not null
+)
+
