44f876af2a9bd018bf7d11f75c4ee4cc12266af0
rt3310 <btac3310@gmail.com>
2022-01-26 12:18:40 +0900

    update user frame

--- boxfolio/boxfolio/src/main/java/centum/boxfolio/controller/user/UserController.java
+++ boxfolio/boxfolio/src/main/java/centum/boxfolio/controller/user/UserController.java
@@ -1,14 +1,29 @@
 package centum.boxfolio.controller.user;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
 
-    @GetMapping("/")
-    public String signup(Model model) {
-        return "/main/home";
+    @GetMapping("/signup")
+    public String signupPage() {
+        return "/user/signup";
+    }
+
+    @PostMapping("/signup")
+    public String signup() {
+        return "/home/home";
+    }
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "/user/login";
+    }
+
+    @PostMapping("/login")
+    public String login() {
+        return "/home/home";
     }
 }
