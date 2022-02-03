6199730f69aa76c2147c78d28526176fed933fd0
hedwig <hedwig3798@gmail.com>
2022-02-03 20:25:08 +0900

    git download develop

--- Spring/testBack/src/test/java/com/example/demo/GitHubApiTest.java
+++ Spring/testBack/src/test/java/com/example/demo/GitHubApiTest.java
@@ -6,29 +6,21 @@ import org.kohsuke.github.GitHub;
 import org.kohsuke.github.GitHubBuilder;
 import org.kohsuke.github.PagedIterable;
 import org.kohsuke.github.GHContent;
-import org.kohsuke.github.GHGistFile;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLDecoder;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.logging.Logger;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @ExtendWith(SpringExtension.class)
@@ -37,7 +29,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class GitHubApiTest {
 	
 	
-	public void downloadDirectory(GHContent content, String path) {
+	public void downloadDirectory(GHContent content, String path, String parentPath) {
 		PagedIterable<GHContent> temp;
 		if (content.isDirectory()) {
 			try {
@@ -52,8 +44,8 @@ public class GitHubApiTest {
 	            	if (i.getName().equals("LICENSE")) {
 	            		continue;
 	            	}
-	            	
-	            	downloadDirectory(i, path + "/" + i.getName());
+
+	            	downloadDirectory(i, path + "/" + i.getName(), path);
 	            	
 	            }
 			} catch (IOException e) {
@@ -61,18 +53,26 @@ public class GitHubApiTest {
 			}
 		}
 		else if (content.isFile()) {
-			downloadFile(content);
+			downloadFile(content, parentPath);
 			return;
 		}
 	}
 	
-	public void downloadFile(GHContent content) {
+	public void downloadFile(GHContent content, String path) {
 		final Logger LOG = Logger.getGlobal();
+
 		LOG.info(content.getName() + " download start");
 		
 		try {
 			String dURL = content.getDownloadUrl();
-			String dir = "D:\\tttt";
+			String dir = "D:\\tttt\\" + path.replace("/","\\");
+			LOG.info(dir);
+			File folder = new File(dir);
+
+			if (!folder.exists()){
+				folder.mkdirs();
+				LOG.info("make"  + dir);
+			}
 			
 			downloadToDir(new URL(dURL), new File(dir));
 			
@@ -80,7 +80,7 @@ public class GitHubApiTest {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
 	
 	public void downloadToDir(URL url, File dir) throws IOException {
@@ -150,7 +150,7 @@ public class GitHubApiTest {
 	    
 
 	    
-	    final String personalToken = "personal token";
+	    final String personalToken = "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";
 		final String repoName = "KPUCE2021SP/HaneulBori";
 	    final Logger LOG = Logger.getGlobal();
 	    PagedIterable<GHRepository> temp;
@@ -181,7 +181,7 @@ public class GitHubApiTest {
 			LOG.info("repo down load success");
 			
 			for (GHContent i : tempFile) {
-            	downloadDirectory(i, i.getName());
+            	downloadDirectory(i, i.getName(), "");
             }
 			
 			LOG.info(repo.getLanguage());
