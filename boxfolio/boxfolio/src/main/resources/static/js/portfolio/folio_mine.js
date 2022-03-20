8f1bd095013a103bc88c02ca4a4dceeeb7092299
kourindou <43141132+kourindou@users.noreply.github.com>
2022-03-21 00:52:12 +0900

    포트폴리오 제작 거의 완성

--- boxfolio/boxfolio/src/main/resources/static/js/portfolio/folio_mine.js
+++ boxfolio/boxfolio/src/main/resources/static/js/portfolio/folio_mine.js
@@ -1,9 +1,15 @@
 var mode=[]; //들어가있는 자료의 종류(사진파일, 영상파일, 유튜브 등). 순서대로. youtube=유투브 영상, video=영상 파일, image=사진 파일, info=설명, git=깃허브 아이디
 var make=[]; //들어가있는 파일의 src나 텍스트
-var make_date='0000_00_00'; //최근 수정 날짜
+var make_date='0000.00.00'; //최근 수정 날짜
 var setting='public'; //공개 설정
 var title='테스트 제목';
 var file_data;
+var file_where='../../image/portfolio';
+var id;
+
+function id_set(input){
+    id=input;
+}
 
 function add(){
     for(var i=0; i<mode.length; i++){
@@ -20,7 +26,8 @@ function add(){
         }
         else if(mode[i]=='image'){ //사진 파일
             var page=document.createElement('img');
-            page.src='../../image/portfolio/1/215B644D56DCF5E82C.jpg';
+            page.src=file_where+'/'+id+'/'+make[i];
+            console.log(file_where+'/'+id+'/'+make[i]);
             page.style.width='1000px';
             page.style.display='flex';
             page.style.marginLeft='auto';
@@ -77,7 +84,6 @@ function add(){
 function result() {
     var jsonData=document.getElementById('value_test').value;
     console.log(jsonData);
-    file_data='C:\\images\\1\\215B644D56DCF5E82C.jpg'//
     var test_data = document.getElementById('file_test').value;
 
     console.log(test_data);
@@ -90,6 +96,7 @@ function result() {
         make[i]=data.index[i].src;
     }
     setting=data.view;
+    make_date=data.date;
     console.log(data);
     add();
 }
